d4815ff46391cbecc148c5922551ff980dd3bdfa
wytg16589 <wytg16589@gmail.com>
2021-02-27 08:27:58 +0000

    V final

--- control_de_flujo.py
+++ control_de_flujo.py
@@ -105,8 +105,9 @@ while tam>0:
 `invertido` (sin hacer uso de la función `reversed` ni del método `reverse`)
 """
 lista2 = list(range(1, 70, 5))
-
-
+invertido=list()
+for a in lista2:
+  invertido.insert(0,a)
 
 
 
@@ -115,7 +116,17 @@ Nota: Un número primo es un número entero que no se puede calcular multiplican
 otros números enteros.
 """
 
-
+lprim=list(range(37,300))
+primos=list()
+for a in lprim:
+  num=a
+  sdiv=0
+  while num>1:
+    if a%num==0:
+      sdiv+=1
+    num-=1
+  if sdiv==1:
+    primos.append(a)
 
 
 
@@ -127,8 +138,12 @@ del segundo cada uno se calcula sumando los dos anteriores términos de la serie
 [0, 1, 1, 2, 3, 5, 8, ...]
 
 """
-
-
+fibonacci=[0,1]
+tam=len(fibonacci)
+while tam<60:
+  term=fibonacci[len(fibonacci)-1]+fibonacci[len(fibonacci)-2]
+  fibonacci.append(term)
+  tam+=1
 
 
 
@@ -141,9 +156,10 @@ Por ejemplo, el factorial de 5 se calcula así:
 5! = 5 × 4 × 3 × 2 × 1 = 120
 """
 
-
-
-
+n=list(range(30,0,-1))
+factorial=1
+while len(n)>0:
+  factorial*=n.pop()
 
 """Guarde en lista `pares` los elementos de la siguiente lista que esten 
 presentes en posiciones pares, pero solo hasta la posición 80.
@@ -151,16 +167,23 @@ presentes en posiciones pares, pero solo hasta la posición 80.
 
 lista3 = [941, 149, 672, 208, 99, 562, 749, 947, 251, 750, 889, 596, 836, 742, 512, 19, 674, 142, 272, 773, 859, 598, 898, 930, 119, 107, 798, 447, 348, 402, 33, 678, 460, 144, 168, 290, 929, 254, 233, 563, 48, 249, 890, 871, 484, 265, 831, 694, 366, 499, 271, 123, 870, 986, 449, 894, 347, 346, 519, 969, 242, 57, 985, 250, 490, 93, 999, 373, 355, 466, 416, 937, 214, 707, 834, 126, 698, 268, 217, 406, 334, 285, 429, 130, 393, 396, 936, 572, 688, 765, 404, 970, 159, 98, 545, 412, 629, 361, 70, 602]
 
-
-
-
+pos=0
+pares=list()
+while pos<81:
+  if pos%2==0:
+    pares.append(lista3[pos])
+  pos+=1
 
 
 """Guarde en lista `cubos` el cubo (potencia elevada a la 3) de los números del 
 1 al 100. 
 """
 
-
+ncub=list(range(1,101))
+cubos=list()
+for a in ncub:
+  val=a**3
+  cubos.append(val)
 
 
 
@@ -168,7 +191,13 @@ lista3 = [941, 149, 672, 208, 99, 562, 749, 947, 251, 750, 889, 596, 836, 742, 5
 y guardar resultado en variable `suma_2s` 
 """
 
-
+nu='2'
+tam=1
+suma_2s=0
+while tam<=10:
+  reg=int(nu*tam)
+  suma_2s+=reg
+  tam+=1
 
 
 
@@ -192,7 +221,22 @@ cantidad máxima de asteriscos de 30.
 **
 *
 """
-
+pat='*'
+linea=1
+longitud=30
+patron=''
+while linea<=longitud:
+  patron+=pat*linea+'\n'
+  linea+=1
+while linea>0:
+  if linea==1:
+    patron+=pat
+    linea-=1  
+  elif linea>=longitud:
+    linea-=1
+  else:
+    patron+=pat*linea+'\n'
+    linea-=1
 
 
 
